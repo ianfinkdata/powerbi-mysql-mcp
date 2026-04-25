3bebc1a2ae23bacd36628d694ec86f31d6146f99
Ian Fink <247514964+ianfinkdata@users.noreply.github.com>
2026-04-24 22:55:45 -0500

    updated bronze and wrote cte for eda

--- sql/bronze_accounts.sql
+++ sql/bronze_accounts.sql
@@ -1,4 +1,6 @@
+/*
 
+CREATE TABLE IF NOT EXISTS bronze_accounts as
 select 
   a.account_id, 
   a.account_name, 
@@ -11,9 +13,27 @@ from accounts as a
 ;
 
 
+select * from bronze_accounts;
+*/
+with total_sales_by_state as (
+select 
+  a.state_province, 
+  a.country, 
+  count(*) as clients,
+  coalesce(sum(o.amount),0) as total_revenue
+  from bronze_accounts as a
+  left join opportunities as o on a.account_id = o.account_id 
+    and o.stage = 'Closed Won'
+  group by 
+  a.state_province, 
+  a.country
+  
+)
 
+-- select * from accounts;
 
-
+select * from total_sales_by_state order by total_revenue desc 
+;
 
 
 
